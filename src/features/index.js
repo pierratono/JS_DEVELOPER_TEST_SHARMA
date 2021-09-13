b12edf9f92574a947402712d871600f144e07e89
Pierratono Costa MAHORO <pierratonoc@gmail.com>
2021-09-13 12:53:10 +0200

    feat-filter

--- src/features/index.js
+++ src/features/index.js
@@ -1,14 +1,37 @@
-//FEATURE Filter option
-const filterData = (params, data) => {
-  console.log(`FEATURE`, "This is filter feature");
-  console.log(`data`, data);
-  console.log(`params`, params);
+//-- filter
+const filterData = (keyword, data) => {
+  const { data: rawData } = data; //Destruct raw data
+
+  return rawData.reduce((prevAnimal, currentAnimal) => {
+    //   filter animals from  owners
+    const animalOwners = currentAnimal.people.reduce((prevPeople, currentPeople) => {
+      const filteredAnimals = currentPeople.animals.filter((animal) =>
+        animal.name.includes(keyword)
+      );
+      if (filteredAnimals.length) {
+        prevPeople.push(
+          Object.assign({}, currentPeople, {
+            animals: filteredAnimals,
+          })
+        );
+      }
+      return prevPeople;
+    }, []);
+    if (animalOwners.length) {
+      prevAnimal.push(
+        Object.assign({}, currentAnimal, {
+          people: animalOwners,
+        })
+      );
+    }
+    return prevAnimal;
+  }, []);
 };
 
 //-- count
 const countData = (data) => {
-  const { data: rawData } = data;
-
+  const { data: rawData } = data; //Destruct raw data
+  // Map and return count included object
   const animalCounts = rawData.map((animalCount) => ({
     name: `${animalCount.name} [${animalCount.people.length}]`,
     people: animalCount.people.map((person) => ({
