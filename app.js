e7f59585f816519cf4a85057541d2a92250a317e
Pierratono Costa MAHORO <pierratonoc@gmail.com>
2021-09-13 12:53:01 +0200

    feat-logger

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const data = require("./src/store/data.js"); // Data in .json
 const { messages } = require("./src/utils"); // Error messages
 const { countData, filterData } = require("./src/features"); // Features fn()
 
+// define json Logger
+const logger = (value) => {
+  console.log(JSON.stringify(value, undefined, 2));
+};
+
 //Get arguments
 let args = process.argv[2];
 
@@ -11,10 +16,10 @@ const handler = (args) => {
   // --filter arg
   if (args && args.includes("--filter")) {
     const params = args.split("=")[1];
-    return params ? filterData(params, data) : console.log(messages[1000]);
+    return params ? logger(filterData(params, data)) : console.log(messages[1000]);
   }
   // --count arg
-  if (args && args.includes("--count")) return console.log(countData(data));
+  if (args && args.includes("--count")) return logger(countData(data));
   // default error arg
   return console.log(messages[2000]);
 };
