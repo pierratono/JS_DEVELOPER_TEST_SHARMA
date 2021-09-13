cdf419437f2f0ee170087c486dbdeae72a6621ff
Pierratono Costa MAHORO <pierratonoc@gmail.com>
2021-09-13 11:48:45 +0200

    add-comments

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 // Import dataset
-const data = require("./src/store/data.js");
-const { messages } = require("./src/utils");
-const { countData, filterData } = require("./src/features");
+const data = require("./src/store/data.js"); // Data in .json
+const { messages } = require("./src/utils"); // Error messages
+const { countData, filterData } = require("./src/features"); // Features fn()
 
 //Get arguments
 let args = process.argv[2];
 
 // Define arguments handler
 const handler = (args) => {
+  // --filter arg
   if (args && args.includes("--filter")) {
     const params = args.split("=")[1];
     return params ? filterData(params, data) : console.log(messages[1000]);
   }
-
+  // --count arg
   if (args && args.includes("--count")) return countData(data);
-
+  // default error arg
   return console.log(messages[2000]);
 };
 
-//Call handler
-
+//Initiate handler
 handler(args);
